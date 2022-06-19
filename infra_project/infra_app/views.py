72f2a6fd42d9c51507b8f7b13c8fef98f6caf88d
Иван Сенькин <plixplox@yandex.ru>
2022-06-19 13:27:07 +0300

    deploy check 4

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось залить проект на боевой сервер!')
+    return HttpResponse('YESSS!')
 
 
 def second_page(request):
